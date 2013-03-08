be8e57f29170da3a8ea58e4decae51be075f59ff
tnek <kentma@nyu.edu>
2013-03-07 21:14:41 -0500

    small changes

--- dwm/dwmstatus.sh
+++ dwm/dwmstatus.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+###
+#   Personal DWM Statusbar Script
+#   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#   :copyright: (C) 2012 by Kent Ma
+#
+###
+
 #: Seperator to divide each value on the statusbar.
 sep="-"
 
-#: Gmail Mail Notifications
-#: Will display the number if unread emails if there are any.
-#: Set the directory for your login information for your email below:
-login_dir=$HOME/.backup/login
+#: Set the directory for your login information for your email:
 #: At $login_dir, your information should be in the below format:
 #:     username="your_username"
 #:     password="your_password"
+login_dir=$HOME/.backup/login
+
+#: Gmail Mail Notifications
+#: Will display the number if unread emails if there are any.
 getEmail()
 {
     if host mail.google.com &> /dev/null; then
@@ -17,20 +25,29 @@ getEmail()
         fullcount=$(awk -F '</?fullcount>' 'NF>1{print $2}' $HOME/.mailcache)
         if [[ "$fullcount" > '0' ]]; then
             echo -en "$sep Emails: $fullcount "
-        else
-            echo -en ""
         fi
+    fi
+}
+
+#: Wi-Fi information
+#: Displays the currently connected network, and the signal strength.
+#: Since this uses $ wicd-cli, obviously it only works if you're using wicd.
+#: No network is shown if you aren't connected to a network.
+getWifi()
+{
+    if [[ $(wicd-cli --wireless -d | grep Essid | wc -l) > 0 ]]; then
+        essid=$(wicd-cli --wireless -d | grep Essid | sed 's/Essid\: //g')
+        str=$(wicd-cli --wireless -d | grep Quality | sed 's/Quality\: //g')
+        echo -ne "$sep Network: $essid: Strength: $str% "
     fi
 }
 
 #: MPD Now Playing Widget
 #: If no song is playing, it will not display anything. As such, this should be 
-#: accounted for when setting your statusbar.
+#: accounted for when setting your statusbar..
 getMpd()
 {
-    if [ "`mpc | sed '2q;d' | grep playing | wc -l`" != 1 ]; then
-        echo -en ""
-    else
+    if [[ ! "`mpc | sed '2q;d' | grep playing | wc -l`" != 1 ]]; then
         mpctime="`mpc | sed '2q;d' | grep playing | awk '{ print $3 }'`"
         echo -en "$sep `mpc | head -1 | cut -c 1-200` [$mpctime] "
     fi
@@ -44,12 +61,15 @@ getDate()
     echo -en $(date +"%a %b %d %r")
 }
 
-#: DWM's statusbar is printed as the value of `xsetroot -name`
-#: This is somewhat a hackish way of making a statusbar.
+# DWM's statusbar is printed as the value of `xsetroot -name`
+# This is somewhat a hackish way of making a statusbar.
 while true; do
-    unread=$(getEmail)
-    for i in {1..300}; do  # Update every 300 seconds, or every 5 minutes
-        xsetroot -name  "${unread}$(getMpd)$sep $(getDate) $sep" 
+    # These two will update every minute instead of every second. 
+    # Hackish way of creating this delay, there may be better methods.
+    wifi=$(getWifi)
+    email=$(getEmail)
+    for i in {1..60}; do
+        xsetroot -name "${email}$(getMpd)${wifi}$sep $(getDate) $sep" 
         sleep 1s
     done
 done &
